a6d40019ec0f7ff895609019d41380314d5cf3e1
Craig Harman <craig@Craigs-MacBook-Pro.local>
2022-05-19 21:27:23 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Nuxtus CLI
+
+Command line interface for the Nuxtus Boilerplate.
+
+> Should ony be used inside the Nuxtus project.
+
+## Usage
+
+```bash
+$ ./nuxtus [command] [options]
+```
+```
+
+## Development
+
+To build the JS:
+
+```bash
+$ npm run build
+```
+
+To package:
+
+```bash
+$ npm run package
+```
---
+++ src/cli.ts
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+
+import { Command } from "commander"
+import chalk from 'chalk'
+import clear from 'clear'
+import figlet from 'figlet'
+
+const program = new Command()
+
+clear()
+console.log(
+	chalk.green(figlet.textSync("nuxtus-cli", { horizontalLayout: "full" }))
+)
+program.name("Nuxtus").version("1.0.0").description("Contact Management System")
+
+program
+	.command("addContact")
+	.alias("a")
+	.description("Add a contact")
+	.action(() => {
+		console.log(
+			chalk.yellow("=========*** Contact Management System ***==========")
+		)
+		// inquirer.prompt(questions).then((answers) => actions.addContact(answers))
+	})
+
+program.parse()
