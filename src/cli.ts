d1099d4d0d14a4f18d19c7a982844cec8ca8698e
Craig Harman <324026+craigharman@users.noreply.github.com>
2022-06-29 19:36:59 +0800

    fix: :bug: -V now reports correction version
    Fixed issue where version was a static string, now it comes from the package.json value.

--- src/cli.ts
+++ src/cli.ts
@@ -3,10 +3,11 @@
 import "dotenv/config"
 
 import { Command } from "commander"
-import chalk from 'chalk'
-import clear from 'clear'
+import chalk from "chalk"
+import clear from "clear"
 import create from "./commands/create"
 import figlet from "figlet"
+import { version } from "../package.json"
 
 const program = new Command()
 
@@ -16,7 +17,10 @@ clear()
 console.log(
 	chalk.green(figlet.textSync("nuxtus-cli", { horizontalLayout: "full" }))
 )
-program.name("nuxtus").version("1.0.0").description("Nuxtus boilerplate CLI")
+program
+	.name("nuxtus")
+	.version("v" + version)
+	.description("Nuxtus boilerplate CLI")
 
 program
 	.command("create")
