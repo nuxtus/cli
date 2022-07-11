929aa432179dee9f2d970227fb0adc7d0407d7bd
Craig Harman <324026+craigharman@users.noreply.github.com>
2022-07-11 21:05:55 +0800

    fix: :bug: use @nuxtus/generator 1.1.2
    @nuxtus/generator 1.1.1 did not include dist folder.

--- test/create.test.ts
+++ test/create.test.ts
@@ -1,32 +1,15 @@
-import { afterEach, assert, expect, test, vi } from "vitest"
+import { afterEach, expect, test } from "vitest"
 
-import chalk from "chalk"
 import { createPage } from "@nuxtus/generator"
-
-// import inquirer from "inquirer" // https://stackoverflow.com/questions/49862039/how-to-write-unit-tests-for-inquirer-js
-
-const path = require("path")
-const fs = require("fs")
+import fs from "node:fs"
 
 afterEach(() => {
-	const pageFolder = path.join("pages", "test")
-	fs.rmdirSync(pageFolder, { recursive: true }, (err) => {
-		if (err) {
-			throw err
-		}
-	})
-	const pageFolder2 = path.join("pages", "test2")
-	fs.rmdirSync(pageFolder2, { recursive: true }, (err) => {
-		if (err) {
-			throw err
-		}
-	})
+	fs.rmSync("pages", { recursive: true })
 })
 
-test("Create collection pages", () => {
-	// vi.mock("inquirer")
-	// inquirer.prompt = vi.fn().mockResolvedValue({ email: "some@example.com" })
-	createPage("test", false, chalk)
+test("Create collection pages", async () => {
+	fs.mkdirSync("pages")
+	await createPage("test", false)
 	expect(fs.existsSync("pages/test")).toBe(true)
 	expect(fs.existsSync("pages/test/index.vue")).toBe(true)
 	const indexPage = fs.readFileSync("pages/test/index.vue")
@@ -41,8 +24,9 @@ test("Create collection pages", () => {
 	).toBe(true)
 })
 
-test("Create singleton page", () => {
-	createPage("test2", true, chalk)
+test("Create singleton page", async () => {
+	fs.mkdirSync("pages")
+	await createPage("test2", true)
 	expect(fs.existsSync("pages/test2/index.vue")).toBe(true)
 	const indexPage = fs.readFileSync("pages/test2/index.vue")
 	expect(indexPage.includes('collection: "test2"')).toBe(true)
