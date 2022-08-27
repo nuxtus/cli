ef97d539f5aafe44d2e76826b624d6de2158a06d
Craig Harman <324026+craigharman@users.noreply.github.com>
2022-08-27 09:01:26 +0800

    test: :white_check_mark: add test for static token usage
    Directus static token test created

--- src/commands/token.ts
+++ src/commands/token.ts
@@ -1,6 +1,6 @@
 import * as CLI from "clui"
 
-import { existsSync, writeFileSync } from "fs"
+import { existsSync, readFileSync, writeFileSync } from "fs"
 
 import Chalk from "chalk"
 import { Command } from "../interfaces/command.interface"
@@ -22,12 +22,21 @@ const token: Command = async function (
 	const user = nuxtus.generateStaticToken()
 	// add the token to nuxt.config.ts
 	const configFile = path.join(process.cwd(), "nuxt.config.ts")
+	const envFile = path.join(process.cwd(), ".env")
 	if (existsSync(configFile)) {
-		const { default: defineNuxtConfig } = await import(configFile);
-		defineNuxtConfig.directus['token'] = user.token
+		// NOTE: Parsing as string for now, might be a better way
+		let configString = readFileSync(configFile, "utf8")
+		configString = configString.replace('directus: {', `directus: {\n\ttoken: "${user.token}"\n`)
 		// Write the config back to a file
-		writeFileSync(configFile, JSON.stringify(defineNuxtConfig, null, 2))
-		// TODO: Remove directus email/password from .env
+		writeFileSync(configFile, configString)
+		// Remove directus email/password from client/.env and add NUXT_PUBLIC_DIRECTUS_TOKEN
+		const envString = readFileSync(envFile, "utf8")
+		const envArray: String[] = envString.split("\n")
+		const newEnvArray = envArray.filter((line) => {
+			return !line.startsWith('NUXT_PUBLIC_DIRECTUS_EMAIL') && !line.startsWith('NUXT_PUBLIC_DIRECTUS_PASSWORD')
+		})
+		newEnvArray.push(`NUXT_PUBLIC_DIRECTUS_TOKEN=${user.token}`)
+		writeFileSync(envFile, newEnvArray.join("\n"))
 		return
 	}
 	
