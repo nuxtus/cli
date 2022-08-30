0f204895dfa9b26e3b6fc731145e76da190ac85a
Craig Harman <324026+craigharman@users.noreply.github.com>
2022-08-30 19:59:38 +0800

    feat: :sparkles: if token already exists in .env, overwrite it
    Check for existence of token in nuxt.config.ts and .env and overwrite if present

--- src/commands/token.ts
+++ src/commands/token.ts
@@ -32,18 +32,23 @@ const token: Command = async function (
 	}
 	// NOTE: Parsing as string for now, might be a better way
 	let configString = readFileSync(configFile, "utf8")
-	configString = configString.replace('directus: {', `directus: {\n\t\ttoken: process.env.NUXT_PUBLIC_DIRECTUS_TOKEN\n\t`)
-	// Write the config back to a file
-	writeFileSync(configFile, configString)
+	if (configString.indexOf(`directus: {\n\t\ttoken:`)===-1) {
+		configString = configString.replace('directus: {', `directus: {\n\t\ttoken: process.env.NUXT_PUBLIC_DIRECTUS_TOKEN\n\t`)
+		// Write the config back to a file
+		writeFileSync(configFile, configString)
+	} else {
+		console.warn(chalk.yellow("Directus token already exists in nuxt.config.ts, skipping (this is only an issue if not using .env variable)."))
+	}
+	
 
 	if (!existsSync(envFile)) {
-		throw new Error("Unable to locate nuxt.config.ts");
+		throw new Error("Unable to locate ,env");
 	}
 	// Remove directus email/password from client/.env and add NUXT_PUBLIC_DIRECTUS_TOKEN
 	const envString = readFileSync(envFile, "utf8")
 	const envArray: String[] = envString.split("\n")
 	const newEnvArray = envArray.filter((line) => {
-		return !line.startsWith('NUXT_PUBLIC_DIRECTUS_EMAIL') && !line.startsWith('NUXT_PUBLIC_DIRECTUS_PASSWORD')
+		return !line.startsWith('NUXT_PUBLIC_DIRECTUS_EMAIL') && !line.startsWith('NUXT_PUBLIC_DIRECTUS_PASSWORD') && !line.startsWith('NUXT_PUBLIC_DIRECTUS_TOKEN')
 	})
 	newEnvArray.push(`NUXT_PUBLIC_DIRECTUS_TOKEN=${data.token}`)
 	writeFileSync(envFile, newEnvArray.join("\n"))
